25134a981e6e0e64d9e716e9ad0eaa6781577d9e
EmmaK <emmaculatewkamau@gmail.com>
2022-04-23 10:09:30 +0300

    Create save credential method

--- credentials.py
+++ credentials.py
@@ -1,2 +1,15 @@
 class Credential:
+    #Empty list that holds our credentials
+    credential_list = []
+    #Define properties for the object credential
+    def __init__(self,account,username,email,password):
+        self.account = account
+        self.username = username
+        self.email = email
+        self.password = password
+
+    #Save Credential
+    def save_credential(self):
+        Credential.credential_list.append(self)
+
     pass
