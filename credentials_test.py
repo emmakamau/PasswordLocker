aecde7e08e0a24b0d3c7365330e9fa14d4f6230e
EmmaK <emmaculatewkamau@gmail.com>
2022-04-23 07:14:47 +0300

    Test for new credential object and reset credential list to zero

--- credentials_test.py
+++ credentials_test.py
@@ -4,4 +4,27 @@ BDD - Basis of our tests
 2. Save a credential
 3. Display credential
 4. Delete credential
+    *Credential should have:
+        a.)Name of the account
+        b.)Username
+        c.)Email address
+        d.)Password
 """
+
+import unittest #test framework
+from credentials import Credential #import class
+
+class TestCredential(unittest.TestCase):
+    # Create credential object
+    def setUp(self):
+        self.new_credential = Credential(
+            "Twitter","janedoe","jane@example.com","J@n3%2D0#"
+        )
+
+    # Teardown mthd resets credential list after every testcase
+    def tearDown(self):
+        Credential.credential_list = []
+
+
+if __name__ == '__main__':
+    unittest.main()
