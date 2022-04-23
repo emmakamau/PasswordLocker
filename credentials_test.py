f2de3158ea56ecf8732fbf8cf1dada3d9786b229
EmmaK <emmaculatewkamau@gmail.com>
2022-04-23 07:04:21 +0300

    Establish BDD for expectated behaviour

---
+++ credentials_test.py
@@ -0,0 +1,7 @@
+"""
+BDD - Basis of our tests
+1. Create a credential
+2. Save a credential
+3. Display credential
+4. Delete credential
+"""
---
+++ user_test.py
@@ -0,0 +1,7 @@
+"""
+BDD - Basis of our tests
+1. We should be able to create a user.
+2. Save the user
+    * User has a username and password
+    * Credentials from the credentials class
+"""
