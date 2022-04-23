4f012d98e98b24be434eeb17c278434e32059fff
EmmaK <emmaculatewkamau@gmail.com>
2022-04-23 12:53:37 +0300

    Test for multiple users and delete user

--- user.py
+++ user.py
@@ -1,6 +1,7 @@
 from credentials import Credential
 
 class User():
+    user_list =[]
     def __init__(self,username,password,credentials:Credential):
         self.username = username
         self.password = password
--- user_test.py
+++ user_test.py
@@ -10,6 +10,7 @@ import unittest
 from user import User
 
 class TestUser(unittest.TestCase):
+    
 
     def setUp(self):
         self.new_user = User("janedoe","j@Z3603",[])
@@ -19,6 +20,29 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.username,"janedoe")
         self.assertEqual(self.new_user.password,"j@Z3603")
         self.assertEqual(self.new_user.credentials,[])
+
+    #Test to check if user is saved
+    def test_save_user(self):
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
+
+    #Test to check if we can have multiple users
+    def test_save_multiple_users(self):
+        self.new_user.save_user()
+        test_user = User("johndoe","J0hzpO3",[])
+        test_user.save_user()
+
+        self.assertEqual(len(User.user_list),2)
+
+    #Test if a user can be deleted
+    def test_delete_user(self):
+        self.new_user.save_user()
+        test_user = User("johndoe","J0hzpO3",[])
+        test_user.save_user()
+        test_user.delete_user()
+
+        self.assertEqual(len(User.user_list),1)
+
 
 if __name__ == '__main__':
     unittest.main()
